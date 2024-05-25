210e8526d44c642afc024577b7c3de20003bea0d
abhijeet1681 <abhijeet.1083846@ratnamcollege.edu>
2024-05-25 19:57:20 +0530

    abhijeet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Logo from "./assets/images/png-clipart-logo-brand-font-food-product-restaurant-logo-design-food-label-thumbnail-removebg-preview.png"
+import Header from "./components/Header";
+import Body from "./components/Body";
+// import Footer123 from "./components/Footer";
 
-const App = () =>{
+
+const App = () => {
   return (
-    <div>This is my react project</div>
+    <>
+      <Header />
+      <Body />
+      {/* <Footer123 /> */}
+    </>
   );
-}
+};
 
 export default App;
