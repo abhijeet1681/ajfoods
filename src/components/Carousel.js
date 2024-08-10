8f61992dc631d9d953b29f789739dbd70ab2511e
abhijeet1681 <abhijeet.1083846@ratnamcollege.edu>
2024-08-11 01:15:51 +0530

    lecture-17 commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,67 +6,373 @@ export default Carousel
 
 
 
-// // CarouselComponent.js
-// import React from 'react';
-// import Slider from 'react-slick';
+// // // CarouselComponent.js
+// // import React from 'react';
+// // import Slider from 'react-slick';
 
-// const CarouselComponent = () => {
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 3,
-//           slidesToScroll: 1,
-//           infinite: true,
-//           dots: true
-//         }
-//       },
-//       {
-//         breakpoint: 600,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 1,
-//           initialSlide: 2
-//         }
-//       },
-//       {
-//         breakpoint: 480,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1
-//         }
-//       }
-//     ]
-//   };
+// // const CarouselComponent = () => {
+// //   const settings = {
+// //     dots: true,
+// //     infinite: true,
+// //     speed: 500,
+// //     slidesToShow: 3,
+// //     slidesToScroll: 1,
+// //     responsive: [
+// //       {
+// //         breakpoint: 1024,
+// //         settings: {
+// //           slidesToShow: 3,
+// //           slidesToScroll: 1,
+// //           infinite: true,
+// //           dots: true
+// //         }
+// //       },
+// //       {
+// //         breakpoint: 600,
+// //         settings: {
+// //           slidesToShow: 2,
+// //           slidesToScroll: 1,
+// //           initialSlide: 2
+// //         }
+// //       },
+// //       {
+// //         breakpoint: 480,
+// //         settings: {
+// //           slidesToShow: 1,
+// //           slidesToScroll: 1
+// //         }
+// //       }
+// //     ]
+// //   };
 
-//   return (
-//     <div>
-//       <h2> Delivery Carousel </h2>
-//       <Slider {...settings}>
-//         <div>
-//           <img src="path/to/your/image1.jpg" alt="Image 1" />
-//         </div>
-//         <div>
-//           <img src="path/to/your/image2.jpg" alt="Image 2" />
-//         </div>
-//         <div>
-//           <img src="path/to/your/image3.jpg" alt="Image 3" />
-//         </div>
-//         <div>
-//           <img src="path/to/your/image4.jpg" alt="Image 4" />
-//         </div>
-//         <div>
-//           <img src="path/to/your/image5.jpg" alt="Image 5" />
-//         </div>
-//       </Slider>
-//     </div>
-//   );
-// }
+// //   return (
+// //     <div>
+// //       <h2> Delivery Carousel </h2>
+// //       <Slider {...settings}>
+// //         <div>
+// //           <img src="path/to/your/image1.jpg" alt="Image 1" />
+// //         </div>
+// //         <div>
+// //           <img src="path/to/your/image2.jpg" alt="Image 2" />
+// //         </div>
+// //         <div>
+// //           <img src="path/to/your/image3.jpg" alt="Image 3" />
+// //         </div>
+// //         <div>
+// //           <img src="path/to/your/image4.jpg" alt="Image 4" />
+// //         </div>
+// //         <div>
+// //           <img src="path/to/your/image5.jpg" alt="Image 5" />
+// //         </div>
+// //       </Slider>
+// //     </div>
+// //   );
+// // }
 
-// export default CarouselComponent;
+// // export default CarouselComponent;
+
+
+// //Shekhar
+// import { useState } from "react";
+// import { Dish_Url } from "../const/config";
+// import { Category } from "./Category";
+
+// export const Carousal = ({
+//   restaurantDish,
+//   restaurantCollection,
+//   deals,
+//   topPicks,
+//   title,
+// }) => {
+//   const [scrollDish, setScrollDish] = useState(0);
+//   const [scrollDeal, setScrollDeal] = useState(0);
+//   const [scrollPick, setScrollPick] = useState(0);
+
+//   const [scrollRes, setScrollRes] = useState(0);
+//   //   const [carousal, setcarousal] = useState(true);
+
+//   return restaurantDish ? (
+//     <>
+//       <div>
+//         <div className="border-bottom pb-5 ">
+//           <div className="d-flex justify-content-between">
+//             <div>
+//               <h4 className="fw-bold col-12">What's on your mind?</h4>
+//             </div>
+//             <div className="d-flex">
+//               <button
+//                 className="carousal-btn mx-1"
+//                 type="button"
+//                 onClick={() => {
+//                   if (scrollDish >= 3) {
+//                     setScrollDish(scrollDish - 3);
+//                   } else {
+//                     console.log("exceed");
+//                   }
+//                 }}
+//                 disabled={scrollDish < 3}
+//               >
+//                 <i className="fa-solid fa-arrow-left " />
+//               </button>
+//               <button
+//                 className="carousal-btn mx-1"
+//                 onClick={() => {
+//                   if (scrollDish <= 6) {
+//                     setScrollDish(scrollDish + 3);
+//                   } else {
+//                     console.log("exceed");
+//                   }
+//                 }}
+//                 disabled={scrollDish > 6}
+//               >
+//                 <i className="fa-solid fa-arrow-right "></i>
+//               </button>
+//             </div>
+//           </div>
+//           <div className="d-flex overflow-hidden ">
+//             {restaurantDish &&
+//               restaurantDish.map((dish, index) => {
+//                 // console.log(dish);
+//                 return (
+//                   <div
+//                     key={`O${index}`}
+//                     className=" col-2"
+//                     style={{
+//                       transform: `translateX(-${scrollDish * 200}%)`,
+//                       transition: "transform 2s",
+//                     }}
+//                   >
+//                     <img
+//                       key={dish.id}
+//                       className=" pe-5  cards-dish"
+//                       src={Dish_Url + dish?.imageId}
+//                       alt={dish?.accessibility?.altText}
+//                     ></img>
+//                   </div>
+//                 );
+//               })}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   ) : restaurantCollection ? (
+//     <>
+//       <div className="border-bottom py-5">
+//         <div className="d-flex justify-content-between">
+//           <div>
+//             <h4 className="fw-bolder col-12">What's on your mind?</h4>
+//           </div>
+//           <div>
+//             <button
+//               className="carousal-btn mx-1"
+//               onClick={() => {
+//                 if (scrollRes >= 3) {
+//                   setScrollRes(scrollRes - 3);
+//                 } else {
+//                   console.log("exceed");
+//                 }
+//               }}
+//               disabled={scrollRes < 3}
+//             >
+//               <i className="fa-solid fa-arrow-left"></i>
+//             </button>
+//             <button
+//               className="carousal-btn mx-1"
+//               onClick={() => {
+//                 if (scrollRes <= 9) {
+//                   setScrollRes(scrollRes + 3);
+//                 } else {
+//                   console.log("exceed");
+//                 }
+//               }}
+//               disabled={scrollRes > 9}
+//             >
+//               <i className="fa-solid fa-arrow-right "></i>
+//             </button>
+//           </div>
+//         </div>
+//         <div className="d-flex overflow-hidden">
+//           {restaurantCollection &&
+//             restaurantCollection.map((restaurant, index) => {
+//               // console.log(dish);
+//               return (
+//                 <div
+//                   key={`T${index}`}
+//                   className=" col-3 py-3 pe-4 border-0 "
+//                   style={{
+//                     transform: `translateX(-${scrollRes * 134}%) `,
+//                     transition: "transform 2s",
+//                   }}
+//                 >
+//                   <Category key={`H${index}`} {...restaurant?.info} />
+//                 </div>
+//               );
+//             })}
+//         </div>
+//       </div>
+//     </>
+//   ) : deals ? (
+//     <>
+//       <div className="p-2 px-3 pt-4">
+//         <div className="d-flex pb-3 justify-content-between">
+//           <div>
+//             <h4 className="fw-bolder col-12">Deals for you</h4>
+//           </div>
+//           <div>
+//             <button
+//               className="carousal-btn  mx-1"
+//               onClick={() => {
+//                 // console.log(deals?.length);
+//                 if (scrollDeal >= 2) {
+//                   setScrollDeal(scrollDeal - 2);
+//                 } else {
+//                   console.log("exceed");
+//                 }
+//               }}
+//               disabled={scrollDeal < 2}
+//             >
+//               <i className="fa-solid fa-arrow-left"></i>
+//             </button>
+//             <button
+//               className="carousal-btn mx-1"
+//               onClick={() => {
+//                 // console.log(scrollDeal);
+//                 if (scrollDeal <= 2) {
+//                   setScrollDeal(scrollDeal + 2);
+//                 } else {
+//                   console.log("exceed");
+//                 }
+//                 // console.log(scrollDeal);
+//               }}
+//               disabled={scrollDeal > 2}
+//             >
+//               <i className="fa-solid fa-arrow-right "></i>
+//             </button>
+//           </div>
+//         </div>
+//         <div className="d-flex   overflow-hidden">
+//           {deals.map((deal, index) => {
+//             // console.log(deal[0]);
+//             return (
+//               <div
+//                 key={index}
+//                 className="me-3   p-2 col-5 d-flex  border  rounded-4"
+//                 style={{
+//                   transform: `translateX(-${scrollDeal * 100}%) `,
+//                   transition: "transform 2s",
+//                 }}
+//               >
+//                 <div className="pe-3">
+//                   <img
+//                     className=""
+//                     src={deal[0].icon}
+//                     width={48}
+//                     height={48}
+//                     alt="Items At ₹169"
+//                   />
+//                 </div>
+//                 <div className="pe-5">
+//                   <h6 className="fw-bolder">
+//                     <strong>{deal[0].title1}</strong>
+//                   </h6>
+//                   <h6 className="text-secondary">
+//                     <small>{deal[0].title2}</small>
+//                   </h6>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </div>
+//     </>
+//   ) : topPicks ? (
+//     <>
+//       <div>
+//         <div className="d-flex pt-4 justify-content-between">
+//           <div>
+//             <h5 className="fw-bolder col-12">{title}</h5>
+//           </div>
+//           <div>
+//             <button
+//               className="carousal-btn mx-1"
+//               onClick={() => {
+//                 if (scrollPick >= 2) {
+//                   setScrollPick(scrollPick - 2);
+//                 } else {
+//                   console.log("exceed");
+//                 }
+//               }}
+//               disabled={scrollPick < 2}
+//             >
+//               <i className="fa-solid fa-arrow-left"></i>
+//             </button>
+//             <button
+//               className="carousal-btn mx-1"
+//               onClick={() => {
+//                 // console.log(scrollDeal);
+//                 if (scrollPick <= 2) {
+//                   setScrollPick(scrollPick + 2);
+//                 } else {
+//                   console.log("exceed");
+//                 }
+//               }}
+//               disabled={scrollPick > 2}
+//             >
+//               <i className="fa-solid fa-arrow-right "></i>
+//             </button>
+//           </div>
+//         </div>
+//         {topPicks && (
+//           <div className="p-1 d-flex overflow-hidden">
+//             {topPicks.map((topPick, index) => {
+//               // console.log(topPick);
+//               return (
+//                 <div
+//                   key={`Q${index}`}
+//                   className=" m-0 me-3 my-3 row col-lg-4 "
+//                   // key={(index) => {
+//                   //   console.log(`a${index}`);
+//                   //   return index;
+//                   // }}
+//                   style={{
+//                     transform: `translateX(-${scrollPick * 50}%) `,
+//                     transition: "transform 2s",
+//                     position: "relative",
+//                   }}
+//                 >
+//                   <img
+//                     className=" p-0  rounded"
+//                     // style={{ width: "100%" }}
+//                     src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/${topPick?.creativeId}`}
+//                     alt={topPick?.title}
+//                   ></img>
+//                   <div
+//                     className="text-light"
+//                     style={{
+//                       position: "absolute",
+//                       bottom: "0%",
+//                     }}
+//                   >
+//                     <div className="d-flex py-3 justify-content-between align-items-center">
+//                       <span className="fw-medium">
+//                         &#8377;
+//                         {(topPick?.dish?.info?.defaultPrice ||
+//                           topPick?.dish?.info?.price) / 100}
+//                       </span>
+//                       <button
+//                         className="btn btn-light fw-bold text-success px-4"
+//                         type="button"
+//                       >
+//                         ADD
+//                       </button>
+//                     </div>
+//                   </div>
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   ) : null;
+// };
---
+++ src/components/ReSearchBar.js
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+// import { TextField, InputAdornment } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export const ResSearchBar = ({ setrestaurantList, restaurantCollection }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    // const filterSearch = () => {
+    if (searchTerm !== "") {
+      const filterData = restaurantCollection.filter((restaurant) => {
+        return (
+          restaurant?.info?.name
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase()) ||
+          restaurant?.info?.cuisines.some((cuisine) => {
+            return cuisine.toLowerCase().includes(searchTerm.toLowerCase());
+          })
+        );
+      });
+      // console.log(filterData);
+
+      setrestaurantList(filterData);
+    } else {
+    }
+    // };
+  }, [searchTerm, restaurantCollection, setrestaurantList]);
+
+  return (
+    <>
+      <div className="searchbar ">
+        {/* Search Bar */}
+        <div className=" my-4">
+          <Link to="/searchbar" className="text-decoration-none text-dark">
+            <div
+              className="rounded border p-2 d-flex  align-items-center"
+              style={{
+                backgroundColor: "#f2f2f3",
+                position: "relative",
+                color: "#6c757d",
+              }}
+            >
+              <button
+                type="button"
+                className="btn p-1  flex-grow-1 text-center btn-block"
+                style={{
+                  color: "#6c757d",
+
+                  border: "none",
+                }}
+              >
+                Search for dishes
+              </button>
+              <i
+                className="fa-solid fa-magnifying-glass "
+                style={{ position: "absolute", top: "35%", right: "2%" }}
+              ></i>
+            </div>
+          </Link>
+        </div>
+        {/* <TextField
+          className=""
+          id="outlined-basic"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          variant="outlined"
+          fullWidth
+          label="Search your restaurant or food here"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment
+                position="end"
+                // className="border rounded-pill"
+              >
+                <i className="fas fa-search" />
+              </InputAdornment>
+            ),
+          }}
+        /> */}
+        {/* <input
+          className="form-control border rounded-pill px-3 m-0 "
+          type="search"
+          id="searchbar"
+          placeholder="search your restaurant or food here"
+          value={searchTerm}
+          onChange={handleSearch}
+          // onKeyDown={(e) => {
+          //   if (e.key === "Enter") {
+          //     filterSearch();
+          //   }
+          // }}
+        >
+          <i className="fa fa-search" aria-hidden="true"></i>
+        </input> */}
+        {/* <input className="form-control border rounded-pill px-3 m-0">
+          {" "}
+          <i className="fa fa-search" aria-hidden="true"></i>
+        </input> */}
+        {/* <button
+          type="button"
+          className="btn btn-primary rounded-pill rounded-start"
+          // onClick={filterSearch}
+        >
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </button> */}
+      </div>
+    </>
+  );
+};
