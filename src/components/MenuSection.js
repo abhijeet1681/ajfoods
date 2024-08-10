f3cf118d7b5063ae09fbc219da08bcf381770880
abhijeet1681 <abhijeet.1083846@ratnamcollege.edu>
2024-08-11 00:55:46 +0530

    lecture-17 commit

--- src/components/MenuSection.js
+++ src/components/MenuSection.js
@@ -1,20 +1,51 @@
-import { HostUrl } from "../constants/restaurants"
-// component for rendering menu component under menu details
-const MenuSection=({isVeg,name,cost,avgRating,ratingCount,description,imgUrl})=>{
+// import { HostUrl } from "../constants/restaurants"
+// // component for rendering menu component under menu details
+// const MenuSection=({isVeg,name,cost,avgRating,ratingCount,description,imgUrl})=>{
+//     return(
+//         <div className="d-flex justify-content-between">
+//             <div>
+//                 <p>{isVeg?"🟢":"🔴"}</p>
+//                 <h6>{name}</h6>
+//                 <h6>{cost}</h6>
+//                 <h6 className="text-success">⭐{avgRating} <span className="text-secondary">{ratingCount}</span></h6>
+//                 <p className="text-secondary">{description}</p>
+//             </div>
+//             <div>
+//                 <img src={HostUrl + imgUrl} alt="" style={{width:"200px",height:"200px"}}/>
+//             </div>
+//         </div>
+//     )
+
+// }
+// export default MenuSection;
+
+
+
+import { IMG_URL } from "../const/config"
+
+const MenuSection = ({isVeg, name, costForTwo, avgRating, ratingCount, description, imageUrl, handleAdd}) =>{
+    
     return(
-        <div className="d-flex justify-content-between">
+        // <div className="d-flex p-2 justify-content-between align-items-center">
+        <div className="d-flex p-2 justify-content-between">
             <div>
-                <p>{isVeg?"🟢":"🔴"}</p>
-                <h6>{name}</h6>
-                <h6>{cost}</h6>
-                <h6 className="text-success">⭐{avgRating} <span className="text-secondary">{ratingCount}</span></h6>
+                <div className="ab">
+                {/* <p>{isVeg ? "🟢" : "🔴"}</p> */}
+                <p>{isVeg ?
+                <img style={{width:"30px", height:"30px", borderRadius:"0px"}} src="../veg.png"/> :
+                <img style={{width:"30px", height:"30px", borderRadius:"0px"}} src="../non-veg.png"/>}</p></div>
+                <h6>{name}</h6> 
+                <h6>Rs. {costForTwo}</h6>
+                <p className="text-success">⭐{avgRating} <span className="text-secondary">({ratingCount})</span></p>
                 <p className="text-secondary">{description}</p>
             </div>
             <div>
-                <img src={HostUrl + imgUrl} alt="" style={{width:"200px",height:"200px"}}/>
+                <img style={{width:"150px", height:"150px", objectFit:"cover", borderRadius:"20px"}} src={IMG_URL+imageUrl}/>
+                <br/><br/>
+                <button onClick={handleAdd}>Add to cart</button>
             </div>
         </div>
     )
-
 }
-export default MenuSection;
+
+export default MenuSection
--- src/components/ResInfo.js
+++ src/components/ResInfo.js
@@ -1,13 +1,38 @@
-const Resinfo = ({name, avgRating, cuisines, deliveryTime,costForTwo, ratingCount, remark}) =>{
-    return(
-        <div>
-            <h4>{name}</h4>
-            <h6>⭐{avgRating}({ratingCount})  <span>-</span>  {costForTwo}</h6>
-            <h6 className="text-danger text-decoration-underline">{cuisines}</h6>
-            <h6 className="text-lowercase">{deliveryTime}</h6>
-            <p>🚲{remark.replace(/<\/?b>/g, "")}.</p>
-        </div>
-    )
-}
+// const ResInfo = ({name, avgRating, cuisines, deliveryTime,costForTwo, ratingCount, remark}) =>{
+//     return(
+//         <div>
+//             <h4>{name}</h4>
+//             <h6>⭐{avgRating}({ratingCount})  <span>-</span>  {costForTwo}</h6>
+//             <h6 className="text-danger text-decoration-underline">{cuisines}</h6>
+//             <h6 className="text-lowercase">{deliveryTime}</h6>
+//             <p>🚲{remark.replace(/<\/?b>/g, "")}.</p>
+//         </div>
+//     )
+// }
 
-export default Resinfo;
+// export default ResInfo;
+
+
+const ResInfo = ({
+    name,
+    avgRating,
+    cuisines,
+    deliveryTime,
+    costForTwo,
+    ratingCount,
+    remark,
+  }) => {
+    return (
+      <div className="info_container p-3">
+        <h4>{name}</h4>
+        <h6>
+          ⭐{avgRating}({ratingCount}) <span>-</span> {costForTwo}
+        </h6>
+        <h6 className="text-danger text-decoration-underline">{cuisines}</h6>
+        <h6 className="text-lowercase">{deliveryTime}</h6>
+        {remark && <p>🚲{remark.replace(/<\/?b>/g, "")}.</p>}
+      </div>
+    );
+  };
+  
+  export default ResInfo;
